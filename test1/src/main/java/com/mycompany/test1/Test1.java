d24782dfd1271da6582ec0e016bbdd4ea911ec4e
Karen <ebo.k@northeastern.edu>
2024-09-28 21:46:43 -0400

    Java Swing Commit

---
+++ test1/src/main/java/com/mycompany/test1/Test1.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.test1;
+
+/**
+ *
+ * @author eboka
+ */
+/* public class Test1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+} */
+
+
+public class Test1 {
+    public static void main(String[] args){
+        int a = 2; 
+        a++;
+        System.out.println(""+a);
+        
+        double b = 10.2;
+        b = b * 2;
+        System.out.println(""+b);
+        
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b1);
+        System.out.println(b2);
+        
+        String name = "Tapaswi";
+        name = name + " Satyapanthi";
+        
+        System.out.println(name);
+    }
+} 
+
